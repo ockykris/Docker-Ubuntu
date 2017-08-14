06e33a5f854c7b11ecc9eae68a8f813a41fd8027
Ocky Kristanto <ocky.kristanto@tomtom.com>
2017-08-14 15:24:34 +0800

    Added Xenial Branch

--- BuildUbuntu.sh
+++ BuildUbuntu.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker build -t kristant/trusty-ubuntu --rm .
+docker build -t kristant/xenial-ubuntu --rm .
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
+FROM ubuntu:xenial
 
 LABEL maintainer : Ocky Kristanto <ockykris@gmail.com>
 
